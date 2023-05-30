84a6a151d98f55f7b3d1f00c18aa8fab8cca1f5d
SchizoChadNoPills <kilian.audouin@sfr.fr>
2023-05-30 16:56:49 +0200

    feat: mise-en-place

---
+++ view/part/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html>
+<?php require_once(realpath(__DIR__ . "/../../config.inc.php"));
+require_once(realpath(__DIR__ . '/../../class/autoloader.php'));
+Autoloader::register();
+?>
+
+<head>
+    <link rel="stylesheet" href="<?= SITE_URL; ?>/assets/css/bootstrap.min.css">
+    <link href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= SITE_URL; ?>/assets/css/style.css">
+    <script src="https://code.jqurney/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="<?= SITE_URL; ?>/assets/js/bootstrap.bundle.min.js"></script>
+    <meta charset="UTF-8">
+</head>
+
+<body>
